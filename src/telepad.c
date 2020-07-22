56291751431afd9a28da42d8cea5b24641f4d81a
Jeffreychen99 <jeffrey_chen99@berkeley.edu>
2020-07-22 15:11:09 -0700

    Implement register changing (buggy)

--- src/telepad.c
+++ src/telepad.c
@@ -15,13 +15,46 @@
 #include <unistd.h>
 #include <mach/mach.h>
 #include <mach/thread_status.h>
+#include <pthread.h>
 
 #include "tele.h"
 
 #define BUFFER_SIZE 1024
 
 void 
-telereturn() {
+set_thread(x86_thread_state64_t *thread_state) {
+
+	// Set registers to the telestruct passed in
+	__asm__ __volatile__("mov %0, %%rax\n\t" : /* no output */ : "a" (thread_state->__rax) : );
+	__asm__ __volatile__("mov %0, %%rbx\n\t" : /* no output */ : "a" (thread_state->__rbx) : );
+	__asm__ __volatile__("mov %0, %%rcx\n\t" : /* no output */ : "a" (thread_state->__rcx) : );
+	__asm__ __volatile__("mov %0, %%rdx\n\t" : /* no output */ : "a" (thread_state->__rdx) : );
+	__asm__ __volatile__("mov %0, %%rdi\n\t" : /* no output */ : "a" (thread_state->__rdi) : );
+	__asm__ __volatile__("mov %0, %%rsi\n\t" : /* no output */ : "a" (thread_state->__rsi) : );
+	__asm__ __volatile__("mov %0, %%rbp\n\t" : /* no output */ : "a" (thread_state->__rbp) : );
+	/*
+	__asm__ __volatile__("mov %0, %%r8\n\t" :  : "a" (thread_state->__r8) : );
+	__asm__ __volatile__("mov %0, %%r9\n\t" :  : "a" (thread_state->__r9) : );
+	__asm__ __volatile__("mov %0, %%r10\n\t" :  : "a" (thread_state->__r10) : );
+	__asm__ __volatile__("mov %0, %%r11\n\t" :  : "a" (thread_state->__r11) : );
+	__asm__ __volatile__("mov %0, %%r12\n\t" :  : "a" (thread_state->__r12) : );
+	__asm__ __volatile__("mov %0, %%r13\n\t" :  : "a" (thread_state->__r13) : );
+	__asm__ __volatile__("mov %0, %%r14\n\t" :  : "a" (thread_state->__r14) : );
+	__asm__ __volatile__("mov %0, %%r15\n\t" :  : "a" (thread_state->__r15) : );
+	*/
+	//__asm__ __volatile__("mov %%rip, %0\n\t" : /* no output */ : "a" (thread_state->__rip) : );
+	//__asm__ __volatile__("mov %%rflags, %0\n\t" : /* no output */ : "a" (thread_state->__rflags) : );
+	__asm__ __volatile__("mov %0, %%cs\n\t" : /* no output */ : "a" (thread_state->__cs) : );
+	__asm__ __volatile__("mov %0, %%fs\n\t" : /* no output */ : "a" (thread_state->__fs) : );
+	__asm__ __volatile__("mov %0, %%gs\n\t" : /* no output */ : "a" (thread_state->__gs) : );
+}
+
+
+
+void
+telereturn(void *args) {
+	set_thread((x86_thread_state64_t *) args);
+	return;
 }
 
 int
@@ -70,6 +103,8 @@ telepad(char *ip) {
     	printf("  + %u threads to set up \n", p_registers->thread_count);
     }
 
+    pthread_t threads[p_registers->thread_count];
+
  	p_registers->start_node = malloc(sizeof(struct thread_list_node));
 	struct thread_list_node *curr_node = p_registers->start_node;
 	for (int i = 0; i < p_registers->thread_count; i++) {
@@ -80,6 +115,7 @@ telepad(char *ip) {
 	    	printf("	  got:      %d \n", bytes_read);
 	    } else {
 	    	printf("  + thread (%d) received \n", i);
+	    	pthread_create(&threads[i], NULL, (void *)&telereturn, &curr_node->state);
 	    }
 
 		if (i < p_registers->thread_count - 1) {
@@ -91,7 +127,11 @@ telepad(char *ip) {
     // Create TeleInfo struct
 	struct TeleInfo *teleInfo = malloc(sizeof(struct TeleInfo));
 
-    telereturn();
+	// Send TeleInfo back
+	for (int i = 0; i < p_registers->thread_count; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
 	return teleInfo;
 }
 
